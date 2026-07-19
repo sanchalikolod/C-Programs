b474165b9e78fbb3ceffd8b6aca1f018659faf7b
sanchalikolod <sanchalikolod@gmail.com>
2026-07-19 22:00:45 +0530

    Added alphabet program

---
+++ Patterns/letter_triangle.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,s; char ch='A';
+	printf("Alphabet triangle");
+	printf("\n\nEnter number of rows: ");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(s=n;s>=i;s--)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			printf("%c ",ch);
+			
+		}
+		printf("\n"); ch++;
+		
+	}
+}
